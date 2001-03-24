eb8d49416484eb416770fafee5e1f0d7fc7ea71c
TOMITA Masahiro <tommy@tmtm.org>
2001-03-25 00:00:00 +0900

    2.3.2a

--- t/50update.rb
+++ t/50update.rb
@@ -1,2 +1,2 @@
 $my.query("update test set id=0, str='hoge'")
-if $my.info != "Rows matched: 2  Changed: 2  Warnings: 0" then raise "update: failed" end
+if $my.affected_rows != 2 then raise "update: failed" end
