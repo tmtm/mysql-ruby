61898a5927b47452f60429c68fdd10456b1e3dc2
TOMITA Masahiro <tommy@tmtm.org>
2002-03-31 00:00:00 +0900

    2.4.2a

--- extconf.rb
+++ extconf.rb
@@ -1,9 +1,9 @@
 require 'mkmf'
 inc, lib = dir_config('mysql', '/usr/local')
-find_library('mysqlclient', 'mysql_query', lib, "#{lib}/mysql") or exit 1
-# If you have error such as 'undefined symbol', delete '#' mark follow
-# lines:
-#have_library('m')
-#have_library('z')
+libs = ['m','z','socket','nsl']
+while not find_library('mysqlclient', 'mysql_query', lib, "#{lib}/mysql") do
+  exit 1 if libs.empty?
+  have_library(libs.shift)
+end
 have_header('mysql.h') or have_header('mysql/mysql.h') or exit 1
 create_makefile("mysql")
